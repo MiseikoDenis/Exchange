56e29d84f300059c71ecd2ebbbebe18468c852cd
MiseikoDenis <den.miseiko@mail.ru>
2022-10-04 14:15:21 +0300

    Работа с dagger

--- app/src/main/java/com/example/exchange/util/di/RepositoryModule.kt
+++ app/src/main/java/com/example/exchange/util/di/RepositoryModule.kt
@@ -1,4 +1,32 @@
 package com.example.exchange.util.di
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+
+import com.example.exchange.api.CurrencyApiService
+import com.example.exchange.database.CurrencyDao
+import com.example.exchange.database.CurrencyDatabase
+import com.example.exchange.database.CurrencyDatabaseEntity
+import com.example.exchange.database.asDomainModel
+import com.example.exchange.models.Currency
+import com.example.exchange.repository.CurrenciesRepository
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Singleton
+
+@Module(includes = [NetworkModule::class, RoomModule::class])
 class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideRepository(
+        database: CurrencyDatabase,
+        retrofitService: CurrencyApiService
+    ): CurrenciesRepository = CurrenciesRepository(database, retrofitService)
+
+    @Provides
+    fun provideCurrenciesList(repository: CurrenciesRepository) = repository.currencies
 }
--- app/src/main/java/com/example/exchange/util/di/RoomModule.kt
+++ app/src/main/java/com/example/exchange/util/di/RoomModule.kt
@@ -1,5 +1,30 @@
 package com.example.exchange.util.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.exchange.database.CurrencyDao
+import com.example.exchange.database.CurrencyDatabase
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
-class RoomModule {
+@Module
+class RoomModule(private val context: Context) {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(): CurrencyDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            CurrencyDatabase::class.java,
+            "currencies"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCurrencyDao(database: CurrencyDatabase): CurrencyDao {
+        return database.currencyDao
+    }
+
 }
